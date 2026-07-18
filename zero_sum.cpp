ba3f27c11e69d9c3ec1d745d6c11e6b9b9ec09d3
ishami-i <i.irene@alustudent.com>
2026-07-18 17:19:47 +0200

    solved https://codeforces.com/contest/2247/problem/A

---
+++ zero_sum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            sum += x;
+        }
+
+        if (n % 2 == 0 && sum % 4 == 0)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
