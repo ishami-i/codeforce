115c5e817e5a79c0caa5910272d8e55d06112f60
ishami-i <i.irene@alustudent.com>
2026-09-01 11:17:17 +0200

    solved https://codeforces.com/problemset/problem/1971/A

---
+++ my_first_sorting.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while(t--) {
+        int x, y;
+        cin >> x >> y;
+        
+        if(y > x) {
+            cout << x << " " << y << '\n';
+        } else {
+            cout << y << " " << x << '\n';
+        }
+    }
+    return 0;
+}
